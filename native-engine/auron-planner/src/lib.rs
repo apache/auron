cff6f6a09921fa978dd955a04922fe0ac577996c
Tartarus0zm <zhangmang1@163.com>
2026-03-19 21:14:15 +0800

    fix

--- native-engine/auron-planner/src/lib.rs
+++ native-engine/auron-planner/src/lib.rs
@@ -13,8 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-extern crate datafusion_ext_plans;
-
 use std::sync::Arc;
 
 use arrow::datatypes::{DataType, Field, Fields, IntervalUnit, Schema, TimeUnit};
