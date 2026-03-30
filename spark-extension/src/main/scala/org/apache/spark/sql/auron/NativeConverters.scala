0e0baf3e3ef7aa335a239dec3463b6f6eb042253
weimingdiit <weimingdiit@gmail.com>
2026-03-31 00:03:09 +0800

    [AURON #2140] Implement native function of map_concat
    Signed-off-by: weimingdiit <weimingdiit@gmail.com>

--- spark-extension/src/main/scala/org/apache/spark/sql/auron/NativeConverters.scala
+++ spark-extension/src/main/scala/org/apache/spark/sql/auron/NativeConverters.scala
@@ -1074,6 +1074,7 @@ object NativeConverters extends Logging {
         buildExtScalarFunction("Spark_NormalizeNanAndZero", e.children, e.dataType)
 
       case e: CreateArray => buildExtScalarFunction("Spark_MakeArray", e.children, e.dataType)
+      case e: MapConcat => buildExtScalarFunction("Spark_MapConcat", e.children, e.dataType)
 
       case e: CreateNamedStruct =>
         buildExprNode {
