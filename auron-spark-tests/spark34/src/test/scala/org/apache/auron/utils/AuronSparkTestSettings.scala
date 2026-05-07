ea7a71bebf07764fee0ad2c361ca56950de9002b
lyne7-sc <734432041@qq.com>
2026-05-07 23:12:11 +0800

    add functions suites for spark3x

--- auron-spark-tests/spark34/src/test/scala/org/apache/auron/utils/AuronSparkTestSettings.scala
+++ auron-spark-tests/spark34/src/test/scala/org/apache/auron/utils/AuronSparkTestSettings.scala
@@ -16,7 +16,41 @@
  */
 package org.apache.auron.utils
 
+import org.apache.spark.sql._
+
 class AuronSparkTestSettings extends SparkTestSettings {
+  {
+    // Use Arrow's unsafe implementation.
+    System.setProperty("arrow.allocation.manager.type", "Unsafe")
+  }
+
+  enableSuite[AuronDataFrameFunctionsSuite]
+    .exclude("map with arrays")
+    .exclude("map_concat function")
+    .exclude("reverse function - array for primitive type not containing null")
+    .exclude("reverse function - array for primitive type containing null")
+    .exclude("reverse function - array for non-primitive type")
+    .exclude("flatten function")
+    .exclude("SPARK-24734: Fix containsNull of Concat for array type")
+    .exclude("array_insert functions")
+    .exclude("transform keys function - Invalid lambda functions and exceptions")
+
+  enableSuite[AuronDateFunctionsSuite]
+    .exclude("function to_date")
+    .exclude("function date_trunc")
+    .exclude("unsupported fmt fields for trunc/date_trunc results null")
+    .exclude("unix_timestamp")
+    .exclude("to_unix_timestamp")
+    .exclude("SPARK-30766: date_trunc of old timestamps to hours and days")
+
+  enableSuite[AuronMathFunctionsSuite]
+    .exclude("acosh")
+
+  enableSuite[AuronMiscFunctionsSuite]
+
+  enableSuite[AuronStringFunctionsSuite]
+    .exclude("string / binary substring function")
+
   override def getSQLQueryTestSettings: SQLQueryTestSettings = new SQLQueryTestSettings {
     override def getResourceFilePath: String = ""
     override def getSupportedSQLQueryTests: Set[String] = Set.empty
